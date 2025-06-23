23827feb5f77c90e4c978f17ad1b8c1a43f93bc6
Afreen Khan <aafreenkhanzee>
2025-06-23 17:51:28 +0530

    Fix: Add ChromeOptions to BaseTest for CI compatibility

--- src/test/java/org/example/tests/BaseTest.java
+++ src/test/java/org/example/tests/BaseTest.java
@@ -3,6 +3,7 @@ package org.example.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -14,16 +15,51 @@ public class BaseTest {
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        // Use headless mode only if running in CI/CD
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://dev-new-commander.swageazy.com/");
     }
-
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 }
+//package org.example.tests;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeClass;
+//
+//import java.time.Duration;
+//
+//public class BaseTest {
+//    protected WebDriver driver;
+
+//    @BeforeClass
+//    public void setUp() {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.get("https://dev-new-commander.swageazy.com/");
+//    }
+//
+//    @AfterClass
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//}
+
